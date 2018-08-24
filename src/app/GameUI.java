18efd1ff0047420891e5327f10db44a9b2a9cbd3
Jiranan Patrathamakul <jiranan.pat@ku.th>
2018-08-24 18:24:24 +0700

    add another way to build board

--- src/app/GameUI.java
+++ src/app/GameUI.java
@@ -44,3 +44,111 @@ public class GameUI {
         new GameUI().run();
     }
 }
+
+
+//import java.awt.event.MouseEvent;
+//import java.awt.event.MouseListener;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//import java.awt.*;
+//import javax.swing.*;
+//
+//public class GameUI extends JFrame implements Runnable, MouseListener {
+//
+//    private Cell[][] cell = new Cell[20][20];
+//
+//
+//    public GameUI(String title) {
+//        super(title);
+//        initCells();
+//        this.addMouseListener(this);
+//
+//    }
+//
+//    private void initCells() {
+//        for (int x = 0; x < 20; x++) {
+//            for (int y = 0; y < 20; y++) {
+//                cell[x][y] = new Cell( x * 40, y * 40);
+//            }
+//        }
+//    }
+//
+//    public void paint(Graphics graphics) {
+//        super.paintComponents(graphics);
+//
+//        //bg
+//        graphics.setColor(Color.WHITE);
+//        graphics.fillRect(0, 0, 800, 800);
+//
+//        //cell
+//        for (int x = 0; x < 20; x++) {
+//            for (int y = 0; y < 20; y++) {
+//                cell[x][y].paintCell(graphics);
+//            }
+//        }
+//
+//        //grid
+//        graphics.setColor(Color.GREEN);
+//        for (int x = 0; x < 40; x++) {
+//            graphics.drawLine(40 * x, 0, 40 * x, 800);
+//        }
+//        for (int y = 0; y < 40; y++) {
+//            graphics.drawLine(0, 40 * y, 800, 40 * y);
+//        }
+//
+//    }
+//
+//
+//    @Override
+//    public void mouseClicked(MouseEvent e) {
+//    }
+//
+//    @Override
+//    public void mousePressed(MouseEvent events) {
+//        int x = events.getX() / 40;
+//        int y = events.getY() / 40;
+//        cell[x][y].setAlive();
+//
+//        System.out.println("X: " + x + " Y: " + y);
+//
+//    }
+//
+//    @Override
+//    public void mouseReleased(MouseEvent e) {
+//    }
+//
+//    @Override
+//    public void mouseEntered(MouseEvent e) {
+//    }
+//
+//    @Override
+//    public void mouseExited(MouseEvent e) {
+//    }
+//
+//    public static void main(String[] args) {
+//        GameUI game = new GameUI("Conway's Game Of Life");
+//        game.setSize(800, 800);
+////        new Thread(game).start();
+//        game.setLocation(100,50);
+//        game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        game.setUndecorated(true);
+//        game.setVisible(true);
+//        game.requestFocusInWindow();
+//    }
+//
+//    @Override
+//    public void run() {
+//        while (true) {
+//
+//            repaint();
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                Logger.getLogger(GameUI.class.getName()).log(Level.SEVERE, null, e);
+//            }
+//
+//        }
+//
+//    }
+//}
+//
